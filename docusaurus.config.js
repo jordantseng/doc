4610a657bd3bf264642adcc0daa65a8cbf2bc7f4
Jordan <jordantseng1024@gmail.com>
2023-04-02 13:17:17 +0800

    update project name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const organizationName = 'jordantseng';
-const projectName = 'doc';
+const projectName = 'frontendwiz';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
